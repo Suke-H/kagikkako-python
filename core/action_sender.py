582050257346d2ee1b644a2731845c0525917d3f
Suke-H <hondii1997@gmail.com>
2024-02-04 16:33:22 +0900

    feat: #9 ゴール判定

--- core/action_sender.py
+++ core/action_sender.py
@@ -1,11 +1,15 @@
 from components.Player import Player
 from components.Map import Map
+from components.Goal import Goal
 from common._class.Actions import Actions
 
-def send_action(actions: Actions, player: Player, map: Map):
+def send_action(actions: Actions, player: Player, map: Map, goal: Goal):
     # マップ上のプレイヤーを移動
     map.move_player(actions.current_position, actions.next_position)
     map.print_player_and_object_map()
 
     # プレイヤーの行動を送信
     player.move(actions.next_position)
+
+    # ゴール判定
+    goal.detect(map.player_map, map.object_map)
